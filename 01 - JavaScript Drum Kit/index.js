8138ad657cf23bebb621363bde5d816a260417e9
Einar Persson <me@einarpersson.com>
2017-09-25 14:54:01 +0200

    Added comments in 01

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -12,9 +12,23 @@ function playSound(e) {
 
 function removePlayingClass(e) {
     if (e.propertyName === 'transform') {
-        e.target.classList.remove('playing')
+        e.target.classList.remove('playing') // Not: det är e.target som man ska använda
     }
 }
 
 window.addEventListener("keydown", playSound)
 document.querySelectorAll('.key').forEach(k => k.addEventListener("transitionend", removePlayingClass))
+
+/* 
+    <<<< What I've learned: >>>>
+    querySelectorAll och getByClassName... ger inte arrayer utan andra typer av objekt, som dock går att konveretera till arrayer genom Array.From()
+    Man kan även iterera på dem direkt... delvis. Men kanske inte använda map/filter etc.
+
+    <audio> audio.play() och audio.currentTime = 0
+
+    data-.... attribut på html-taggar.
+
+    Kombinera queryselector med string literals :)
+
+    Man kan subscriba till att en css-transition tar slut! Smart!
+ */
